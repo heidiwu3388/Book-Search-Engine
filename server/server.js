d95fb6d6ef19d9340d0bdf14d6104d185575edd8
heidiwu3388 <heidiwu3388@yahoo.com>
2023-03-03 23:50:23 -0800

    Set up an Apollo Server to use GraphQL queries and mutations to fetch and modify data, replacing the existing RESTful API.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
+// import packages
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+// import user defined files
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+// set up express server
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+// create a new instance of Apollo server and pass in our schema data and context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,8 +28,23 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,20 +6,20 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({req}) {
+    // get token from header
+    let token = req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // trim off "Bearer" from the token string and return only the token
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
-    // verify token and get user data out of it
+    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -28,8 +28,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
