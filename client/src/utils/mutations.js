fb3e2d8a448a2be0bf1885f51ba2efc65ae3109f
heidiwu3388 <heidiwu3388@yahoo.com>
2023-03-04 21:12:00 -0800

    define mutations for 'login', 'saveBook' and 'removeBook' for later use by 'useMutation'.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,12 +3,47 @@ import { gql } from '@apollo/client';
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
-        token
-        user {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookId: String!, $description: String!, $title: String!) {
+    saveBook(bookId: $bookId, description: $description, title: $title) {
+        _id
+        username
+        email
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
             _id
             username
             email
-        }
+            }
         }
     }
 `;
